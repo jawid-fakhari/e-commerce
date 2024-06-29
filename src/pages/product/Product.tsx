e516e80e22d51335329306920df0d1ea3620cb03
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-29 17:24:28 +0200

    Product component added

---
+++ src/pages/product/Product.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Container from '../../components/container/Container'
+
+
+function Product() {
+
+    const params = useParams()
+
+  return (
+    <div>
+      <Container>
+        <div className="h-auto shadow mt-4 grid grid-cols-12">
+          <div className="col-span-4 p-4">
+            <img
+              src="https://www.evosmart.it/wp-content/uploads/2023/08/nokia-150-960x540.jpg"
+              alt=""
+            />
+            <button>Add to card</button>
+          </div>
+          <div className="col-span-8 py-2">
+            <h1 className='text-xl font-semibold'>Product Name</h1>
+            <div>
+              <p>Price: 30$</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+                fugiat, commodi, ad error quam magnam recusandae reiciendis ex
+                inventore aliquam quibusdam praesentium, optio quaerat et.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Product
--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import ProductItem from '../../components/productItem/ProductItem'
-import Container from '../../components/container/Container';
+import React from "react";
+import ProductItem from "../../components/productItem/ProductItem";
+import Container from "../../components/container/Container";
+import { Link } from "react-router-dom";
 
 function Store() {
   return (
@@ -8,18 +9,22 @@ function Store() {
       <Container>
         <h1 className="text-left my-5 ">Newest Products</h1>
         <div className="grid grid-cols-4 gap-7">
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
+          <Link to={`/product/${1}`}>
+            <ProductItem />
+          </Link>
+          <Link to={`/product/${2}`}>
+            <ProductItem />
+          </Link>
+          <Link to={`/product/${3}`}>
+            <ProductItem />
+          </Link>
+          <Link to={`/product/${4}`}>
+            <ProductItem />
+          </Link>
         </div>
       </Container>
     </>
   );
 }
 
-export default Store
+export default Store;
