ec7dd1da8859ec2293c6532686687e5e113d70c7
Jawid Fakhari <jawid_fakhari@live.it>
2024-07-01 12:31:52 +0200

    products.map added to loop and render products

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -4,38 +4,30 @@ import Container from "../../components/container/Container";
 import { Link } from "react-router-dom";
 import { getProducts } from "../../services/api";
 
+//pagina Store che mostra tutti i prodotti nello store
+
 function Store() {
   //con useState settiamo i products
   const [products, setProducts] = useState([]);
 
   //all'interno del useEffect importo func getProduct da api.ts e con metodo then prendo dati e setto products dai data ricevuti
-  useEffect(() =>{
+  useEffect(() => {
     getProducts().then((result) => {
-     setProducts(result);
-    })
-    
-  }, [])
-
-  console.log(products);
+      setProducts(result);
+    });
+  }, []);
   
-
+  
   return (
     <>
       <Container>
         <h1 className="text-left my-5 ">Newest Products</h1>
         <div className="grid grid-cols-4 gap-7">
-          <Link to={`/product/${1}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${2}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${3}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${4}`}>
-            <ProductItem />
-          </Link>
+          {products.map((item) => (
+            <Link key={item.id} to={`/product/${item.id}`}>
+              <ProductItem />
+            </Link>
+          ))}
         </div>
       </Container>
     </>
