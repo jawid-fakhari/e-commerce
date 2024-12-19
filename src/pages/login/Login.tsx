2d9723cf6e231d77e51cb0ffc3a13e6fc2e51e07
jawid-fakhari <jawid_fakhari@live.it>
2024-12-19 19:04:20 +0100

    login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import Container from '../../components/container/Container'
-import { useShoppingCartContext } from '../../context/ShoppingCartContext'
+import Container from "../../components/container/Container";
+import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 
 function Login() {
-    const {handleLogin} = useShoppingCartContext();
-  return (
-    <div>
-        <Container>
-            <div className='bg-slate-300 p12 rounded'>
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='password' />
-                <button onClick={handleLogin}>Login</button>
-            </div>
-        </Container>
-    </div>
-  )
+    const { handleLogin } = useShoppingCartContext();
+    return (
+        <div>
+            <Container>
+                <div className="bg-slate-300 p12 rounded">
+                    <input type="text" placeholder="username" />
+                    <input type="password" placeholder="password" />
+                    <button onClick={handleLogin}>Login</button>
+                </div>
+            </Container>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
